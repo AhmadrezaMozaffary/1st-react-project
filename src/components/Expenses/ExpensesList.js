332ee0bd47fc724431da41ef6c347a3eca9afdf6
Ahmadreza Mozaffary <79336004+AhmadrezaMozaffary@users.noreply.github.com>
2022-06-27 12:52:28 +0430

    Add new expenses plus filtering

---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.css";
+
+const ExpensesList = (props) => {
+  if (props.items.length === 0)
+    return <h2 className="expenses-list__fallback">Found No Expenses</h2>;
+
+  return (
+    <li className="expenses-list">
+      {props.items.map((item) => (
+        <ExpenseItem
+          key={item.id}
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+        />
+      ))}
+    </li>
+  );
+};
+
+export default ExpensesList;
